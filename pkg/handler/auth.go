82793aded5cfb9aeeee46f206649fc3d0d3842dc
sultanbekovr <91003424+sultanbekovr@users.noreply.github.com>
2022-10-25 01:40:59 +0600

    added user registration

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -1,9 +1,27 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sultanbekovr/todo-app"
+	"net/http"
+)
 
 func (h *Handler) signUp(c *gin.Context) {
+	var input todo.User
 
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+	}
+
+	id, err := h.service.Authorization.CreateUser(input)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"id": id,
+	})
 }
 
 func (h *Handler) signIn(c *gin.Context) {
